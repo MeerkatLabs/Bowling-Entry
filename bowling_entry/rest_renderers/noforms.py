f274299b3dc761159de7d08c4ed62955f4143e79
Robert Robinson <rerobins@gmail.com>
2015-03-17 12:35:17 -0600

    Remove all forms from django rest browsable API

--- bowling_entry/rest_renderers/noforms.py
+++ bowling_entry/rest_renderers/noforms.py
@@ -5,3 +5,6 @@ class NoFormBrowsableAPIRenderer(BrowsableAPIRenderer):
 
     def get_raw_data_form(self, data, view, method, request):
         return None
+
+    def get_rendered_html_form(self, data, view, method, request):
+        return None
